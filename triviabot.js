b39070c194bf3aa768e04878c9a81f7571586d43
Raevel <github.com@edea.se>
2010-10-21 13:39:22 +0200

    Now uses IrcWrapper adminCommands.

--- triviabot.js
+++ triviabot.js
@@ -1,11 +1,13 @@
 require('./vendor/CactusJuice/CactusJuice');
 var IRC = require('./vendor/IRC-js/lib/irc');
-require('./vendor/IrcWrapper/IrcWrapper');
+var IW = require('./vendor/IrcWrapper/IrcWrapper');
 require('./lib/Question');
 require('./lib/Trivia');
 require('./lib/TriviaRepository');
 var ctb = require('./lib/createTriviaBot');
 module.exports = {
   IRC : IRC,
-  createTriviaBot : ctb.createTriviaBot
+  createTriviaBot : ctb.createTriviaBot,
+  adminCommands : IW.adminCommands
 };
+global.adminCommands = IW.adminCommands;
